0a1c8ff94342a58c9ab2b79c1241b6fbaa735561
Shuto Suga <challcha3737@gmail.com>
2026-09-11 16:48:58 +0900

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# サイトアクセスのため
 import requests
-# htmlを読み込ませるため
 from bs4 import BeautifulSoup
 
+# --練習用--
+
 # jraの公式サイト
-url = "https://www.jra.go.jp/"
+#url = "https://www.jra.go.jp/"
+
+# 練習用レース
+url = "https://www.jra.go.jp/JRADB/accessD.html?CNAME=pw01dde0106202604031120260912/9D"
 
 # 接続確認
 response = requests.get(url, timeout=10)
@@ -12,7 +15,40 @@ response = requests.get(url, timeout=10)
 
 # getしたresponseをhtmlとして解釈
 soup = BeautifulSoup(response.content, "html.parser")
+
 # 文字コード確認
 #print(response.encoding)
+# タイトル出力
 #print(soup.title.text)
 
+#馬の名前を抽出（最初の1匹分）
+#horse = soup.find("td", class_="horse")
+#print(horse)
+#name = horse.find("div", class_="name")
+#print(name.text)
+
+#馬の名前を抽出（全馬分）
+race_table = soup.find(id="syutsuba")
+horses = race_table.find_all("td", class_="horse")
+
+for horse in horses:
+    name = horse.find("div", class_="name")
+
+    if name is None:
+        print("nameが見つからない要素:")
+        print(horse)
+    else:
+        print(name.text)
+print(len(horses))
+
+
+
+
+
+
+
+
+
+
+
+# --練習用ここまで--
